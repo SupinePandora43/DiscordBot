5893bc5cad37f7104aaae4ee9fb4fbcd072448b9
SupinePandora43 <supinepandoraggg@gmail.com>
2020-06-30 20:08:54 +0500

    rule34js

--- src/plugins/rule34.ts
+++ src/plugins/rule34.ts
@@ -1,40 +1,14 @@
 import { Message } from "discord.js"
-import { parse } from "fast-xml-parser"
-import bent from "bent";
 import { setTimeout } from "timers";
-
-const options = {
-	attributeNamePrefix: "",
-	//attrNodeName: false, //default is 'false'
-	textNodeName: "#text",
-	ignoreAttributes: false,
-	ignoreNameSpace: false,
-	allowBooleanAttributes: false,
-	parseNodeValue: true,
-	parseAttributeValue: false,
-	trimValues: false,
-	cdataTagName: "__cdata", //default is 'false'
-	cdataPositionChar: "\\c",
-	parseTrueNumberOnly: false,
-	arrayMode: false, //"strict"
-	stopNodes: ["parse-me-as-string"]
-}
+import { posts } from "rule34js"
 
 export async function on_message(msg: Message) {
 	if (msg.content.startsWith("rule34")) {
-
-		let tags: string | string[] = msg.content.split(" ")
+		let tags = msg.content.split(" ")
 		tags.splice(0, 1)
-		tags = tags.join("+")
-		console.log(tags)
-		const url = `https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${tags}`
-
-		const getString = bent('string')
-		const obj = await getString(url)
-		const json = parse(obj, options, true)
-
-		if (json.posts.post && json.posts.post[0]) {
-			msg.reply(json.posts.post[0].file_url)
+		const tagPost = await posts({ tags: tags })
+		if (tagPost[0]) {
+			msg.reply(tagPost[0].file_url)
 		} else {
 			msg.react("0️⃣")
 			setTimeout(async () => {
